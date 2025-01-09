43b9f74613622d15919f2c99dc236ef663ac00ad
Pedro Henrique Borges Luiz <66181571+Drinpy@users.noreply.github.com>
2025-01-09 16:51:35 -0300

    Update testvar.php

--- testvar.php
+++ testvar.php
@@ -1,4 +1,6 @@
 <?php
-$txt = "descobrir";
-echo "Só há um jeito de $txt!";
+$txt = "descobrir";                       // Entre Aspas salva uma String.
+$num = 2;                                 // Sem Aspas salva um Inteiro.
+echo "Só há um jeito de $txt!". "<br>" ;  // O <br> serve para pular a linha e é necessário colocar '.' anteriormente.
+echo "Há $num jeitos de descobrir!";
 ?>
