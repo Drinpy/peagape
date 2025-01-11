7c7373796aa33d7c873b4019616e3510c108a42d
Pedro Henrique Borges Luiz <66181571+Drinpy@users.noreply.github.com>
2025-01-10 23:15:05 -0300

    Update testvar.php

--- testvar.php
+++ testvar.php
@@ -1,6 +1,8 @@
 <?php
+$sum = 9 + 4; 
 $txt = "descobrir";                       // Entre Aspas salva uma String.
 $num = 2;                                 // Sem Aspas salva um Inteiro.
 echo "Só há um jeito de $txt!". "<br>" ;  // O <br> serve para pular a linha e é necessário colocar '.' anteriormente.
-echo "Há $num jeitos de descobrir!";
+echo "Há $num jeitos de descobrir!". "<br>" ;
+echo "A soma de 4 e 9 é igual a $sum.";
 ?>
