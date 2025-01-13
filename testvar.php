15ea990704eefe8f0b500bba84700d5700f83a5e
Pedro Henrique Borges Luiz <66181571+Drinpy@users.noreply.github.com>
2025-01-13 16:37:51 -0300

    Update testvar.php

--- testvar.php
+++ testvar.php
@@ -1,8 +1,13 @@
 <?php
+// Aqui teremos exemplos do aprendizado com variáveis em PHP!
+
 $sum = 9 + 4; 
 $txt = "descobrir";                       // Entre Aspas salva uma String.
 $num = 2;                                 // Sem Aspas salva um Inteiro.
+
 echo "Só há um jeito de $txt!". "<br>" ;  // O <br> serve para pular a linha e é necessário colocar '.' anteriormente.
 echo "Há $num jeitos de descobrir!". "<br>" ;
 echo "A soma de 4 e 9 é igual a $sum.";
+
+var_dump($num);                           // Apresenta o tipo da variável que eu selecionei. Em STRINGS apresenta o número de caractéres.
 ?>
