661d36aa6ad8da1dffcf6846ff4614443f60a6e4
Pedro Henrique Borges Luiz <66181571+Drinpy@users.noreply.github.com>
2025-01-13 17:35:33 -0300

    Update testvar.php

--- testvar.php
+++ testvar.php
@@ -10,4 +10,17 @@ echo "Há $num jeitos de descobrir!". "<br>" ;
 echo "A soma de 4 e 9 é igual a $sum.";
 
 var_dump($num);                           // Apresenta o tipo da variável que eu selecionei. Em STRINGS apresenta o número de caractéres.
+
+// Var SCOPE
+
+$one = "UM";                              // Variável definida em Global Scope. Fora das chaves da função.
+
+function myTest() {
+                                          // Nenhuma variável foi definida dentro da função.
+  echo "Só há $one jeito de descobrir!";  // Logo, dentro da função 'myTest' a variável não retornará nada!
+}
+
+myTest();
+echo "Só há $one jeito de descobrir!";    // Por estar fora da função a variável Global retornará o valor.
+
 ?>
