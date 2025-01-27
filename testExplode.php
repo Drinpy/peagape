056d36eb7b7b3ddf9b4bb1b7d8c65de8e3f1e482
Pedro Henrique Borges Luiz <66181571+Drinpy@users.noreply.github.com>
2025-01-27 17:40:26 -0300

    Create testExplode.php

---
+++ testExplode.php
@@ -0,0 +1,9 @@
+<?php
+
+$x = "Só há um jeito de descobrir!";
+$y = explode(" ", $x);                // A função 'explode' converte String em Array.
+
+print_r($y);                          // Aqui será apresentada a posição de cada palavra no Array.
+echo "<br>";
+echo($x);                             // Aqui é a String antes sem a função 'explode'.
+?>
